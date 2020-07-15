b312cc9a459b16a8550214202ef1a7f71c65c422
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-15 23:47:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
-def prime? (n)
-  if n == 1 or n == 0 or n < 0
+def prime?(n)
+  if n < 0 or n == 0 or n == 1
     return false
   else n > 1
     (2..n - 1).to_a.all? {|num| n % num != 0}
