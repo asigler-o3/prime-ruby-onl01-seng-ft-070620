81b8d5c259799bbfe83c9301698be7f727fe3aa3
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-15 23:45:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,7 @@ def prime? (n)
   if n == 1 or n == 0 or n < 0
     false
   else n > 1
-    (2..n - 1).to_a.all? {|num| n % num == 0}
+    (2..n - 1).to_a.all? {|num| n % num != 0}
+  end
   end
 end
