ea66f6ec51560d62e12c7c2323cc29efafd476cf
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-15 23:00:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,5 @@ def prime? (array)
   counter = -1
 
   Prime.prime? (-1..101013).to_a do |prime|
-    while array.include?
-      yield array[counter]
-      counter += 1
-    end
-    array
-  end
+    
 end
